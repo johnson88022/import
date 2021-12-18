1494c797865135ebb519b4b74f09294861ea2af6
Johnson88022 <johnson88022!@gmail.com>
2021-12-19 06:06:47 +0800

    finish version

--- randint_02.py
+++ randint_02.py
@@ -1,13 +1,18 @@
 import random
-num  = random.randint(1,100)
-count = 0
+start = input("请输入开始值")
+end = input("请输入开始值")
+start = int(start)
+end = int(end)
+num  = random.randint(start,end)
+count = 0    #计算猜几次数字,若写在回圈内则每次次数都会
 while True:
 	count = count+1  #快写法:count += 1
 	count = int(count)
-	n = input("猜一个数字(范围0~100): ")
+	n = input("猜一个数字: ")
 	n = int(n)
 	if n == num:
 		print("恭喜猜对")
+		print("这是你的猜",count,"次")
 		break
 	elif n > num: 
 		print("比",n,"小")
@@ -16,5 +21,8 @@ while True:
 	elif n < 0 and n > 100:
 		print("范围在0~100哦")
 	print("这是你的猜",count,"次")
+
+
+	
 
 	
