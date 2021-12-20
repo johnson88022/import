19b8c5e13841248b262330282c315498bad9594a
Johnson88022 <johnson88022!@gmail.com>
2021-12-20 09:10:37 +0800

    third_check_version

--- randint_02.py
+++ randint_02.py
@@ -6,7 +6,7 @@ end = int(end)
 num  = random.randint(start,end)
 count = 0    #计算猜几次数字,若写在回圈内则每次次数都会重复1=0+1
 while True:
-	count = count+1  #快写法:count += 1
+	count = count+1  #每跑1次while回圈数字就会加1  #快写法:count += 1  
 	count = int(count)
 	n = input("猜一个数字: ")
 	n = int(n)
@@ -18,9 +18,11 @@ while True:
 		print("比",n,"小")
 	elif n < num:
 		print("比",n,"大")
-	elif n < 0 and n > 100:
-		print("范围在0~100哦")
-	print("这是你的猜",count,"次")
+	print("这是你猜的第",count,"次")
+		
+		
+
+		
 
 
 
